1744eca3212aafb16d40a8bab63854aa7b5cf571
Dan Jacob and Jesse Hoare Evans <jesse@>
2017-03-22 09:22:01 -0700

    add initial html, css, and local variable functions

---
+++ scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event) {
+      $(".person1").text("person1Input");
+      $(".person2").text("person2Input");
+      $(".animal").text("animalInput");
+      $(".exclamation").text("exclamationInput");
+      $(".verb").text("verbInput");
+      $(".noun").text("nounInput");
+
+      $(".person1").text(person1Input);
+      $(".person2").text(person2Input);
+      $(".animal").text(animalInput);
+      $(".exclamation").text(exclamationInput);
+      $(".verb").text(verbInput);
+      $(".noun").text(nounInput);
+
+      $("#story").show(){
+
+      event.preventDefault();
+      });
+
+
+
+
+
+
+})
