f4fd08b113b725799cb22d566cde101e21aa13a8
Dan Jacob and Jesse Hoare Evans <jesse@>
2017-03-22 09:34:56 -0700

    final touches, making everything work

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
-      $(".person1").text("person1Input");
-      $(".person2").text("person2Input");
-      $(".animal").text("animalInput");
-      $(".exclamation").text("exclamationInput");
-      $(".verb").text("verbInput");
-      $(".noun").text("nounInput");
+      var person1Input = $("input#person1").val();
+      var person2Input = $("input#person2").val();
+      var animalInput = $("input#animal").val();
+      var exclamationInput = $("input#exclamation").val();
+      var verbInput = $("input#verb").val();
+      var nounInput = $("input#noun").val();
 
       $(".person1").text(person1Input);
       $(".person2").text(person2Input);
@@ -14,7 +14,7 @@ $(document).ready(function(){
       $(".verb").text(verbInput);
       $(".noun").text(nounInput);
 
-      $("#story").show(){
+      $("#story").show();
 
       event.preventDefault();
       });
